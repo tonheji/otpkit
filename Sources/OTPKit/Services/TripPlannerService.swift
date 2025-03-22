db13f05e9d9f6892be1a9686bd0a63c0f6aa45e1
tonheji <tonheji@gmail.com>
2025-03-22 06:22:17 +0200

    fix main actor

--- Sources/OTPKit/Services/TripPlannerService.swift
+++ Sources/OTPKit/Services/TripPlannerService.swift
@@ -206,7 +206,7 @@ public final class TripPlannerService: NSObject {
     /// Generates markers for the map based on selected points
     ///
     /// - Returns: MapContent containing the markers
-    public func generateMarkers() -> some MapContent {
+    @MainActor public func generateMarkers() -> some MapContent {
         ForEach(Array(selectedMapPoint.values.compactMap { $0 }), id: \.id) { markerItem in
             Marker(item: markerItem.item)
         }
